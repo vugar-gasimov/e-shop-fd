c513535e8e886961367a5384ca76899656f1bae9
vugar-gasimov <vuqar5qasimov@gmail.com>
2024-09-01 20:59:34 +0400

    Header toggle and it is design with new icons and logic for it.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,6 +14,8 @@ import { Link, useLocation } from 'react-router-dom';
 import { PiUserCircleFill } from 'react-icons/pi';
 import { RiLockPasswordFill } from 'react-icons/ri';
 import { TiShoppingCart } from 'react-icons/ti';
+import { BiSolidPhoneCall } from 'react-icons/bi';
+import { TbMailCog } from 'react-icons/tb';
 
 const Header = () => {
   const { pathname } = useLocation();
@@ -29,13 +31,13 @@ const Header = () => {
             <ul className='flex justify-start items-center gap-8 font-semibold text-black'>
               <li className='flex relative justify-center items-center gap-2 text-sm after:absolute after:h-[18px] after:w-[1px] after:bg-[#afafaf] after:-right-[16px]'>
                 <span className=''>
-                  <TfiEmail />
+                  <TfiEmail size={18} />
                 </span>
                 <span className=''>support@gmail.com</span>
               </li>
               <li className='flex relative justify-center items-center gap-2 text-sm'>
                 <span className=''>
-                  <FaPhoneSquareAlt />
+                  <FaPhoneSquareAlt size={18} />
                 </span>
                 <span className=''>+(380) 973 16 97</span>
               </li>
@@ -215,6 +217,172 @@ const Header = () => {
                     </div>
                   </div>
                 </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='hidden md-lg:block'>
+        <div
+          onClick={() => setShowSidebar(true)}
+          className={`fixed duration-200 transition-all ${
+            showSidebar ? 'invisible' : 'visible'
+          } hidden md-lg:block w-screen h-screen bg-[rgba(0,0,0,0.5)] top-0 left-0 z-20`}
+        ></div>
+        <div
+          className={`w-[300px] z-[100] transition-all duration-200 fixed ${
+            showSidebar ? '-left-[300px]' : 'left-0 top-0'
+          } overflow-y-auto bg-white h-screen py-6 px-8`}
+        >
+          <div className='flex justify-start flex-col gap-6'>
+            <Link to='/'>
+              <img
+                src='http://localhost:3000/images/logo.png'
+                alt='Website logo'
+              />
+            </Link>
+            <div className='flex justify-start items-center gap-10'>
+              <div className='flex group cursor-pointer text-slate-800 text-sm justify-center items-center gap-1 relative after:h-[18px] after:w-[1px] after:bg-[#afafaf] after:-right-[16px] after:absolute  '>
+                <img
+                  src='http://localhost:3000/images/language.png'
+                  alt='Language selection flag'
+                />
+                <span>
+                  <TbArrowBigDownFilled />
+                </span>
+                <ul className=' absolute invisible transition-all top-12 rounded-lg duration-200 text-white p-2 w-[100px] flex flex-col gap-3 group-hover:visible group-hover:top-6 bg-black'>
+                  <li>
+                    <button className='focus:bg-indigo-600 focus:outline-none hover:bg-indigo-500 px-2 py-1 rounded w-full text-left'>
+                      English{' '}
+                    </button>
+                  </li>
+                  <li>
+                    {' '}
+                    <button className='focus:bg-indigo-600 focus:outline-none hover:bg-indigo-500 px-2 py-1 rounded w-full text-left'>
+                      German{' '}
+                    </button>
+                  </li>
+                  <li>
+                    {' '}
+                    <button className='focus:bg-indigo-600 focus:outline-none hover:bg-indigo-500 px-2 py-1 rounded w-full text-left'>
+                      Spanish{' '}
+                    </button>
+                  </li>
+                  <li>
+                    {' '}
+                    <button className='focus:bg-indigo-600 focus:outline-none hover:bg-indigo-500 px-2 py-1 rounded w-full text-left'>
+                      Chinese{' '}
+                    </button>
+                  </li>
+                </ul>
+              </div>
+              {user ? (
+                <Link
+                  to='/dashboard'
+                  className='flex cursor-pointer justify-center items-center gap-2 text-sm text-black'
+                >
+                  <span>
+                    <PiUserCircleFill size={20} />
+                  </span>
+                  <span>Chuckle McGiggles</span>
+                </Link>
+              ) : (
+                <Link
+                  to='/login'
+                  className='flex cursor-pointer justify-center items-center gap-2 text-sm text-black'
+                >
+                  <span>
+                    <RiLockPasswordFill size={18} />
+                  </span>
+                  <span>Login</span>
+                </Link>
+              )}
+            </div>
+            <ul className='flex flex-col justify-start items-center gap-4 text-sm font-bold uppercase '>
+              <li>
+                <Link
+                  className={`py-2 block ${
+                    pathname === '/' ? 'text-[#059473]' : 'text-slate-600'
+                  }`}
+                >
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className={`py-2 block ${
+                    pathname === '/shop' ? 'text-[#059473]' : 'text-slate-600'
+                  }`}
+                >
+                  Shop
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className={`py-2 block ${
+                    pathname === '/blog' ? 'text-[#059473]' : 'text-slate-600'
+                  }`}
+                >
+                  Blog
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className={`py-2 block ${
+                    pathname === '/about' ? 'text-[#059473]' : 'text-slate-600'
+                  }`}
+                >
+                  About Us
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className={`py-2 block ${
+                    pathname === '/contact'
+                      ? 'text-[#059473]'
+                      : 'text-slate-600'
+                  }`}
+                >
+                  Contact Us
+                </Link>
+              </li>
+            </ul>
+            <div className='flex justify-center items-center gap-4 text-black'>
+              <a href='https://example.com'>
+                <ImFacebook2 />
+              </a>
+              <a href='https://example.com'>
+                <FaSquareXTwitter size={19} />
+              </a>
+              <a href='https://example.com'>
+                <FaLinkedin size={19} />
+              </a>
+              <a href='https://example.com'>
+                <VscGithub size={19} />
+              </a>
+            </div>
+
+            <ul className='flex flex-col justify-start items-center gap-3 text-[#1c1c1c]'>
+              <li className='flex justify-center items-center gap-2 text-sm'>
+                <span>
+                  <TbMailCog size={28} />
+                </span>
+                <span className='text-sm font-medium text-slate-700'>
+                  support@gmail.com
+                </span>
+              </li>
+            </ul>
+            <div className='w-full flex justify-center ml-[-8px] gap-4 items-center'>
+              <div className='w-[48px] h-[48px] rounded-full flex bg-[#f5f5f5] justify-center items-center'>
+                <span className=''>
+                  <BiSolidPhoneCall size={28} />
+                </span>
+              </div>
+              <div className='flex justify-end flex-col gap-1'>
+                <p className='text-sm font-medium text-slate-700'>
+                  +(380) 973 16 97
+                </p>
+                <p className='text-sm font-bold'>Support 24/7</p>
               </div>
             </div>
           </div>
