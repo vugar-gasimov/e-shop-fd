0007fdc1e946b99cb87219282329f335a4b78bc1
vugar-gasimov <vuqar5qasimov@gmail.com>
2024-09-01 19:20:22 +0400

    Header middle design and fake date plus icons. Tailwind config important fix.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { TfiEmail } from 'react-icons/tfi';
-import { FaLinkedin, FaPhoneSquareAlt } from 'react-icons/fa';
+import {
+  FaHeart,
+  FaLinkedin,
+  FaListUl,
+  FaPhoneSquareAlt,
+} from 'react-icons/fa';
 import { ImFacebook2 } from 'react-icons/im';
 import { FaSquareXTwitter } from 'react-icons/fa6';
 import { VscGithub } from 'react-icons/vsc';
 import { TbArrowBigDownFilled } from 'react-icons/tb';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { PiUserCircleFill } from 'react-icons/pi';
 import { RiLockPasswordFill } from 'react-icons/ri';
+import { TiShoppingCart } from 'react-icons/ti';
 
 const Header = () => {
+  const { pathname } = useLocation();
+  const [showSidebar, setShowSidebar] = useState(true);
   const user = true;
+  const wishList_count = 3;
 
   return (
     <div className='w-full bg-white'>
@@ -102,6 +111,110 @@ const Header = () => {
                     <span>Login</span>
                   </Link>
                 )}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='w-full'>
+        <div className='w-[85%] lg:w-[90%] mx-auto'>
+          <div className='h-[80px] md-lg:h-[100px] flex justify-between items-center flex-wrap'>
+            <div className='md-lg:w-full w-3/12 md-lg:pt-4'>
+              <div className='flex justify-between items-center'>
+                <Link to='/'>
+                  <img
+                    src='http://localhost:3000/images/logo.png'
+                    alt='Website logo'
+                  />
+                </Link>
+
+                <button
+                  type='button'
+                  onClick={() => setShowSidebar(false)}
+                  aria-label='Toggle navigation menu'
+                  className='justify-center items-center w-[30px] h-[30px] bg-white text-slate-600 border border-slate-600 rounded-lg cursor-pointer lg:hidden md-lg:flex xl:hidden hidden'
+                >
+                  <FaListUl />
+                </button>
+              </div>
+            </div>
+            <div className='md-lg:w-full w-9/12'>
+              <div className='flex justify-between md-lg:justify-center items-center flex-wrap pl-8'>
+                <ul className='flex justify-start items-start gap-8 text-sm font-bold uppercase md-lg:hidden'>
+                  <li>
+                    <Link
+                      className={`p-2 block ${
+                        pathname === '/' ? 'text-[#059473]' : 'text-slate-600'
+                      }`}
+                    >
+                      Home
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      className={`p-2 block ${
+                        pathname === '/shop'
+                          ? 'text-[#059473]'
+                          : 'text-slate-600'
+                      }`}
+                    >
+                      Shop
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      className={`p-2 block ${
+                        pathname === '/blog'
+                          ? 'text-[#059473]'
+                          : 'text-slate-600'
+                      }`}
+                    >
+                      Blog
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      className={`p-2 block ${
+                        pathname === '/about'
+                          ? 'text-[#059473]'
+                          : 'text-slate-600'
+                      }`}
+                    >
+                      About Us
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      className={`p-2 block ${
+                        pathname === '/contact'
+                          ? 'text-[#059473]'
+                          : 'text-slate-600'
+                      }`}
+                    >
+                      Contact Us
+                    </Link>
+                  </li>
+                </ul>
+                <div className='flex md-lg:hidden justify-center items-center gap-5'>
+                  <div className='flex justify-center gap-5'>
+                    <div className='relative flex justify-center items-center cursor-pointer w-[35px] h-[35px] rounded-full bg-[#e2e2e2]'>
+                      <button type='button' className='text-xl text-green-500'>
+                        <FaHeart />
+                      </button>
+                      <div className='w-[20px] h-[20px] absolute bg-red-500 rounded-full text-white flex justify-center items-center -top-[3px] -right-[5px]'>
+                        {wishList_count}
+                      </div>
+                    </div>
+                    <div className='relative flex justify-center items-center cursor-pointer w-[35px] h-[35px] rounded-full bg-[#e2e2e2]'>
+                      <button type='button' className='text-xl text-green-500'>
+                        <TiShoppingCart size={24} />
+                      </button>
+                      <div className='w-[20px] h-[20px] absolute bg-red-500 rounded-full text-white flex justify-center items-center -top-[3px] -right-[5px]'>
+                        {wishList_count}
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
